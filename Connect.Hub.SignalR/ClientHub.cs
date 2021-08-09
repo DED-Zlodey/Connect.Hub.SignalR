e065ac416b4c8d974f9cc00ce68b976b439d514b
Zlodey <admin@il2-expert.ru>
2021-08-09 13:04:24 +0800

    Добавьте файлы проекта.

---
+++ Connect.Hub.SignalR/ClientHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace Connect.Hub.SignalR
+{
+    class ClientHub
+    {
+        HubConnection connection;
+        public ClientHub()
+        {
+            Start();
+        }
+        private async void Start()
+        {
+            connection = new HubConnectionBuilder().WithUrl(new Uri("https://localhost:5001/HubDevices")).WithAutomaticReconnect().Build();
+            connection.On("Send", (string message) =>
+            {
+                Console.WriteLine($"Message from {message}");
+            });
+            await connection.StartAsync();
+        }
+        public async void SendMess(string mess)
+        {
+            await connection.InvokeAsync("Send", mess);
+        }
+    }
+}
---
+++ Connect.Hub.SignalR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Connect.Hub.SignalR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientHub client = new ClientHub();
+            var mess = Console.ReadLine();
+            client.SendMess(mess);
+            Console.ReadKey();
+        }
+    }
+}
